fc15562f25738975dfa9e10a84e04d1a9f723602
MichaelDanuEkklasiya <63625736+MichaelDanuEkklasiya@users.noreply.github.com>
2020-09-02 09:39:43 +0700

    Add files via upload

---
+++ Try.php
@@ -0,0 +1,19 @@
+<?php
+class komputer
+{
+	public $prosesor;
+	public $memory;
+
+	public function __construct($prosesor,$memory = "12 Gb")
+	{
+		$this->prosesor = $prosesor;
+		$this->memory = $memory;
+	}
+	public function cetak (){
+		return "$this->prosesor | $this->memory";
+	}
+}
+
+$komputerSaya = new komputer("Core i7","8 GB");
+echo $komputerSaya->Cetak();
+?>
